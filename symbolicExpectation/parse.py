62abc185250fa031942136f625f7d0692612b0f3
krosac <z1557340316@gmail.com>
2018-06-14 02:56:49 -0700

    Update parse.py

--- symbolicExpectation/parse.py
+++ symbolicExpectation/parse.py
@@ -2,9 +2,14 @@ from tensorflow.core.protobuf import meta_graph_pb2
 from collections import defaultdict
 from utils import Graph, bGraph
 from z3 import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("meta", nargs='?', default="check_string_for_empty")
+args = parser.parse_args()
 
 meta_graph = meta_graph_pb2.MetaGraphDef()
-input_file_name = "myGraph.meta"
+input_file_name = args.meta#"myGraph.meta"
 with open(input_file_name, 'rb') as fin:
     file_content = fin.read()
 meta_graph.ParseFromString(file_content)
@@ -36,7 +41,7 @@ st = mg.topologicalSort()
 bg = bGraph(mg, st[-1], nodeIdMap, idNodeMap, predIdMap)
 dist = bg.compute(st[-1])
 for val in dist.keys():
-    print("E[Output=",val,"] = ",dist[val].simplify(), sep="")
+    print("E[Output=",val,"] = ",dist[val], sep="")
 
 '''
 dist_req = defaultdict()
@@ -46,5 +51,4 @@ dist_req[RealVal(2)] = RealVal(0.35)
 res = bg.regroup(dist, 3)
 bg.match(dist, res, dist_req)
 '''
-
 
